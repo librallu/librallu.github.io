87890f889d01075e204bc4193843397daa1fb570
librallu <libralesso.l@gmail.com>
2017-03-25 19:45:32 +0100

    initial commit; old content from v1 + architecture for generating output site. We need now to create the new theme for pelican

---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Luc Libralesso'
+SITENAME = 'Luc Libralesso'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+LOCALE = (
+    'fr',
+    'fr_FR.utf8'
+)
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+
+STATIC_PATHS = ['cv', 'images']
+PAGE_EXCLUDES = ['cv']
+ARTICLE_EXCLUDES = ['cv']
+
+
+THEME='pelican_in'
